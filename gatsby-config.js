6a06973d1109e6d3f94ef00c55f8f8e9184ec0de
chipp972 <pierrecharles.nicolas@gmail.com>
2022-10-14 02:05:21 +0200

    fix: npm start

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,25 @@
-require("dotenv").config();
+require('dotenv').config();
 
 module.exports = {
   siteMetadata: {
-    siteName: 'My Shop',
+    siteName: 'iGyal'
   },
   plugins: [
     'gatsby-plugin-sass',
     'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-datocms`,
-      options: { apiToken: process.env.DATO_API_TOKEN },
-    },
-    {
       resolve: 'gatsby-plugin-snipcart',
       options: {
-        apiKey: 'OWE3MmZmMjQtNTk3Yi00OThhLWEwMmUtZDY4ZWM4NzIwYzZiNjM2NjM0Mzc1NzE0MTUwNzI1',
+        apiKey: process.env.SNIPCART_API_KEY,
         autopop: true
       }
     },
-  ],
-}
+    {
+      resolve: 'gatsby-source-datocms',
+      options: {
+        apiToken: process.env.DATO_API_TOKEN
+      }
+    },
+    'gatsby-plugin-netlify'
+  ]
+};

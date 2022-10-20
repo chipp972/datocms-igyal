1d9d9c2a16ad10cdc0c2810e2f832a8068d9b08a
chipp972 <pierrecharles.nicolas@gmail.com>
2022-10-20 21:41:45 +0200

    fix: Remove conflicting dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,13 +7,13 @@ module.exports = {
   plugins: [
     'gatsby-plugin-sass',
     'gatsby-plugin-react-helmet',
-    {
-      resolve: 'gatsby-plugin-snipcart',
-      options: {
-        apiKey: process.env.SNIPCART_API_KEY,
-        autopop: true
-      }
-    },
+    // {
+    //   resolve: 'gatsby-plugin-snipcart',
+    //   options: {
+    //     apiKey: process.env.SNIPCART_API_KEY,
+    //     autopop: true
+    //   }
+    // },
     {
       resolve: 'gatsby-source-datocms',
       options: {
